e61edb0715a6bf5a333adb32e50dad09417eac40
Mohammed Nishad TK <93390945+nishad7447@users.noreply.github.com>
2024-07-27 12:30:26 +0530

    updated API from render

--- client/src/API.jsx
+++ client/src/API.jsx
@@ -1,2 +1,2 @@
-export const UserBaseURL ='https://api.onlyfriends.fun'
-export const AdminBaseURL ='https://api.onlyfriends.fun/admin'
+export const UserBaseURL ='https://project2-socialmedia.onrender.com'
+export const AdminBaseURL ='https://project2-socialmedia.onrender.com/admin'
